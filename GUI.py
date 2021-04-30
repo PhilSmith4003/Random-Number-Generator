af05503edea270739360be73310b87811b206fbb
Phil <jdlarson47@gmail.com>
2021-04-30 12:12:17 -0500

    GUI Changes made

--- GUI.py
+++ GUI.py
@@ -1,1 +1,10 @@
-from tkinter import *
+import tkinter
+
+
+window = tkinter.Tk(className="Random Number Generator!")
+window.geometry("800x600")
+window.configure(bg='black')
+
+(tkinter.Message(window, aspect=200, text="What range would you like the number to be within?")).pack()
+
+window.mainloop()
